be6eb1e624cb4a83ad55508a0a4bca071c279f2b
NguyenThanhNhan-7068 <162886633+NguyenThanhNhan-7068@users.noreply.github.com>
2025-11-21 08:55:53 +0700

    bai tap ngay 19_11

---
+++ untitled3/src/bai_hoc_ngay_19_11/ToanTu.java
@@ -0,0 +1,20 @@
+package bai_hoc_ngay_19_11;
+
+public class ToanTu {
+    public static void toantu(){
+        int a=3;
+        int b=5;
+        int sum= (a + b);
+        System.out.println("Tong: "+ sum);
+        System.out.println("Hieu: "+(a-b));
+        System.out.println("tich; "+ (a*b));
+        System.out.println("thuong: "+((double)a/b));
+        System.out.println("chia lay du: "+(a%b));
+
+
+    }
+    public static void main(String[] args) {
+        toantu();
+
+    }
+}
---
+++ untitled3/src/bai_tap_ngay_19_11/BanHang.java
@@ -0,0 +1,25 @@
+package bai_tap_ngay_19_11;
+
+import java.util.Scanner;
+
+public class BanHang {
+    public static void hoa_don(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap ten hang: ");
+        String tenHang= sc.nextLine();
+        System.out.println("nhap so luong: ");
+        int soLuong= sc.nextInt();
+        System.out.println("nhap don gia: ");
+        float donGia = sc.nextFloat();
+        float tienHang = soLuong * donGia;
+        System.out.println("tien hang: "+ tienHang);
+        float VAT = tienHang * 0.1f;
+        System.out.println("thue VAT" + VAT);
+        float tongTien = tienHang + VAT;
+        System.out.println("Tong tien thanh toan: "+ tongTien);
+    }
+
+    public static void main(String[] args) {
+        hoa_don();
+    }
+}

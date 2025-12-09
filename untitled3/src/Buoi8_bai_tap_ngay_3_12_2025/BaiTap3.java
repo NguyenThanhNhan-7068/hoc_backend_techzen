098022528499cb7876629f23ef9234c669db7c71
NguyenThanhNhan-7068 <162886633+NguyenThanhNhan-7068@users.noreply.github.com>
2025-12-09 09:45:45 +0700

    bài tập buổi 8

---
+++ untitled3/src/Buoi8_bai_tap_ngay_3_12_2025/BaiTap3.java
@@ -0,0 +1,91 @@
+package Buoi8_bai_tap_ngay_3_12_2025;
+
+import java.util.Scanner;
+
+public class BaiTap3 {
+
+    public static void nhapNhanVien(String[] hoTen, int[] tuoi, String[] gioiTinh, double[] luongCoBan, double[] diemTB, int index, Scanner sc) {
+        sc.nextLine();
+        System.out.print("ho va ten: ");
+        hoTen[index] = sc.nextLine();
+
+        System.out.print("tuoi: ");
+        tuoi[index] = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("gioi tinh: ");
+        gioiTinh[index] = sc.nextLine();
+
+        System.out.print("luong co ban: ");
+        luongCoBan[index] = sc.nextDouble();
+
+        System.out.print("diem trung binh: ");
+        diemTB[index] = sc.nextDouble();
+    }
+
+    public static void xuatNhanVien(String[] hoTen, int[] tuoi, String[] gioiTinh, double[] luongCoBan, double[] diemTB, int index) {
+        System.out.println("----- nhan vien " + (index + 1) + " -----");
+        System.out.println("ten: " + hoTen[index]);
+        System.out.println("tuoi: " + tuoi[index]);
+        System.out.println("gioi tinh: " + gioiTinh[index]);
+        System.out.println("luong co ban: " + luongCoBan[index]);
+        System.out.println("diem trung binh: " + diemTB[index]);
+    }
+
+    public static void sapXepTheoTuoi(String[] hoTen, int[] tuoi, String[] gioiTinh, double[] luongCoBan, double[] diemTB, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (tuoi[i] > tuoi[j]) {
+                    int tempTuoi = tuoi[i];
+                    tuoi[i] = tuoi[j];
+                    tuoi[j] = tempTuoi;
+
+                    String tempTen = hoTen[i];
+                    hoTen[i] = hoTen[j];
+                    hoTen[j] = tempTen;
+
+                    String tempGT = gioiTinh[i];
+                    gioiTinh[i] = gioiTinh[j];
+                    gioiTinh[j] = tempGT;
+
+                    double tempLuong = luongCoBan[i];
+                    luongCoBan[i] = luongCoBan[j];
+                    luongCoBan[j] = tempLuong;
+
+                    double tempDTB = diemTB[i];
+                    diemTB[i] = diemTB[j];
+                    diemTB[j] = tempDTB;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("nhap so luong nhan vien: ");
+        int n = sc.nextInt();
+
+        String[] hoTen = new String[n];
+        int[] tuoi = new int[n];
+        String[] gioiTinh = new String[n];
+        double[] luongCoBan = new double[n];
+        double[] diemTB = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\n=== nhan vien " + (i + 1) + " ===");
+            nhapNhanVien(hoTen, tuoi, gioiTinh, luongCoBan, diemTB, i, sc);
+        }
+
+        System.out.println("\n===== DS nhan vien =====");
+        for (int i = 0; i < n; i++) {
+            xuatNhanVien(hoTen, tuoi, gioiTinh, luongCoBan, diemTB, i);
+        }
+
+        sapXepTheoTuoi(hoTen, tuoi, gioiTinh, luongCoBan, diemTB, n);
+        System.out.println("\n===== DS nhan vien sau khi sap xep theo tuoi =====");
+        for (int i = 0; i < n; i++) {
+            xuatNhanVien(hoTen, tuoi, gioiTinh, luongCoBan, diemTB, i);
+        }
+    }
+}
---
+++ untitled3/src/buoi9_bai_hoc_ngay_8_12_2025/TowDirectionArray.java
@@ -0,0 +1,107 @@
+package buoi9_bai_hoc_ngay_8_12_2025;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TowDirectionArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = {{10,20,30},{40,50},{60,70}};
+        System.out.println(Arrays.toString(arr[0])); //[10, 20, 30]
+        System.out.println(arr[1][1]);  //50
+        System.out.println(arr.length); //3
+        for (int i = 0; i< arr.length; i++){
+            System.out.println(arr[i].length);
+        }
+        for (int i = 0; i< arr.length;i++){
+         System.out.println(Arrays.toString(arr[i]));  // cach in nhanh(1 vong for). cach con lai ben duoi(2vong for).
+//            for (int j = 0;j < arr[i].length;i++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+        }
+
+///      nhap  mang 2 chieu.
+//        buoc 1: nhap vao m hang
+        int m = inputPositive(sc, "m hang");
+//        buoc 2: nhap vap n cot
+        int n = inputPositive(sc,"n cot");
+//        buoc 3: new ramang moi m hang n cot
+        int[][] a = new int[m][n];
+//        buoc 4: tao phuong thuc nhap mang
+        inputArray(sc,a);
+//        buoc 5: xuat mang:
+        outputArray(a);
+
+
+///        cac thao tac kiem tra
+//        1. co phai mang toan chan khong
+        System.out.println("kiem tra mang toan chan: " + isAllEven(a));
+//        2. tao mang 1 chieu X voi X[i] la cac gia tri lon nhat tren tung dong i cua ma tran A, in X ra
+        System.out.println("gia tri cao nhat tren tung dong mang: " + Arrays.toString(maxValuePerRow(a)));
+
+//        3. hoan doi 2 dong ma tran
+        System.out.println("nhap dong 1 muon swap: ");
+        int dong1= sc.nextInt();
+        System.out.println("nhap dong 2 muon swap: ");
+        int dong2 = sc.nextInt();
+        swap(a,dong1,dong2);
+    }
+    public static void swap(int[][] a,int dong1,int dong2){
+        int[] temp = a[dong1];
+        a[dong1] = a[dong2];
+        a[dong2] = temp;
+        System.out.println("hoan doi 2 dong trong ma tran: ");
+        outputArray(a);
+    }
+    public static int[] maxValuePerRow(int[][] a){
+        int[] X = new int[a.length];
+        for (int i = 0; i<a.length;i++){
+            int max = a[i][0];
+            for (int j = 0; j <a[i].length;j++){
+                if(max < a[i][j]) max = a[i][j];
+            }
+            X[i] = max;
+        }
+        return X;
+    }
+    public static boolean isAllEven(int[][] a){
+        for(int i = 0; i<a.length;i++){
+            for (int j = 0; j <a[i].length;j++){
+                if(a[i][j]% 2 != 0) return false;
+            }
+        }
+        return true;
+    }
+    public static void outputArray(int[][] a){
+        for(int i = 0; i<a.length;i++){
+            for (int j =0; j < a[i].length;j++){
+                System.out.printf(a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void inputArray(Scanner sc, int[][] a){
+        for(int i = 0; i<a.length;i++){
+            for (int j =0; j < a[i].length;j++){
+                System.out.printf("nhap phan tu mang a[%d][%d]: ",i,j);
+                a[i][j] =sc.nextInt();
+            }
+        }
+    }
+    //        nhap mang 2 chieu.
+    public static int inputPositive(Scanner sc,String target){
+        int n;
+        do{
+            System.out.printf("Nhap vao %s: ",target);
+            n = sc.nextInt();
+            if(n <= 1 ){
+                System.out.println("so nhap vao phai lon hon 0, nhap lai: ");
+            }
+        }
+        while (n <=1);
+        return n;
+    }
+}
